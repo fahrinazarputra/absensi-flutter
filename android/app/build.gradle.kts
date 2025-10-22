5eb899ec5d691bc3ad569e134fa7e007e508de73
fahrinazarputra <fahrinazar6@gmail.com>
2025-10-22 14:30:34 +0700

    komat komit

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("kotlin-android")
+    id("org.jetbrains.kotlin.android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.absensi"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion "27.0.12077973";
+    ndkVersion = "27.0.12077973" // ✅ gunakan tanda '='
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -16,14 +16,11 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "11" // ✅ lebih aman ditulis seperti ini
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.absensi"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -31,7 +28,7 @@ android {
     }
 
     buildTypes {
-        release {
+        getByName("release") {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
